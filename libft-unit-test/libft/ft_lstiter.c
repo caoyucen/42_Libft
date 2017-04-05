7cf458873cf1c33ba50ddd88ab168d08c44e8e4b
ycao <caoyucen@gmail.com>
2017-04-04 18:53:33 -0700

    fix bugs

--- libft-unit-test/libft/ft_lstiter.c
+++ libft-unit-test/libft/ft_lstiter.c
@@ -4,8 +4,12 @@ void ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
 	if (!lst)
 		return ;
+	f(lst);
 	while (lst->next)
+	{
 		ft_lstiter(lst->next, f);
-	if (lst->next)
-		f(lst);
+		lst = lst->next;
+	}
+	//if (lst->next)
+	//	f(lst);
 }
